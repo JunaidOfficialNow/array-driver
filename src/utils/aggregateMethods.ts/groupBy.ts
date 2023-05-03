c3a7c16a352a37e24db6db4e977ea64d32f0e1a5
Junaid <junaidofficialnow@gmail.com>
2023-05-03 18:02:26 +0530

    bug: fixed the groupBy function

--- src/utils/aggregateMethods.ts/groupBy.ts
+++ src/utils/aggregateMethods.ts/groupBy.ts
@@ -1,29 +1,28 @@
 /**
- * Groups array of objects by one or more keys
- * @param keys One or more keys to group by
- * @returns Object with grouped keys as properties and grouped values as arrays
+ * Groups array of objects by given key 
+ * @param key key to group by
+ * @returns Object with grouped key's values as properties and grouped values as arrays
  */
-    type GroupBy<T, K extends keyof T> =
-    T extends Record<K, infer U extends string | number | symbol>
-      ? Record<string, K extends string ? GroupBy<T, Exclude<keyof T, K>> & { [key in U]: Extract<T, Record<K, key>>[] } : never>
-      : never;
-  
-  function groupBy<T, K extends keyof T>(arr: T[], ...keys: K[]): GroupBy<T, K> {
-    let groups: any = {};
-    const groupBySingleKey = (arr: T[], key: keyof T) => {
-      const result = arr.reduce((acc, item) => {
-        const value = item[key];
-        const group = groups[value] || (groups[value] = []);
-        group.push(item);
-        return acc;
-      }, {} as GroupBy<T, Exclude<keyof T, K>>);
-      return { ...result, ...groups };
-    };
-    return keys.reduce((result, key) => {
-      groups = {};
-      return result && groupBySingleKey(result, key);
-    }, arr) as unknown as GroupBy<T, K>;
+
+
+interface MyObject {
+  [key: string]: any;
+}
+
+function groupBy<T extends MyObject>(this: T[], key: string ): Record<string, T[]> {
+  if (key === undefined) {
+    return {}
   }
+  return this.reduce((acc, curr)=> {
+    if (curr[key] !== undefined) {
+      if (!acc[curr[key]]) {
+        acc[curr[key]] = [];
+      }
+      acc[curr[key]].push(curr);
+    }
+    return acc;
+  }, {} as Record<string , T[]>)
+}
   
 
 
