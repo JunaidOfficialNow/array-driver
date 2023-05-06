72401789b4750a840f0c7fdfbef9c518e066f5ea
Junaid <junaidofficialnow@gmail.com>
2023-05-06 19:02:26 +0530

    feat: improved the edge cases of distinct method

--- src/utils/filterMethods/distinct.ts
+++ src/utils/filterMethods/distinct.ts
@@ -2,6 +2,6 @@
  * Returns a new array with only unique values from the original array.
  */
 export function distinct<T>(this: T[]): T[] {
-  return Array.from(new Set(this));
+  return Array.from(new Set(this.map((el)=> JSON.stringify(el)))).map((el)=> JSON.parse(el));
 }
 
