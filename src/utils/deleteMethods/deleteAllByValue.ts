0cdb2bf9adaf177c40db757ebe253c62f658947a
Junaid <junaidofficialnow@gmail.com>
2023-05-13 18:55:43 +0530

    feat: improved and optimized the deleteAllByValue function

--- src/utils/deleteMethods/deleteAllByValue.ts
+++ src/utils/deleteMethods/deleteAllByValue.ts
@@ -3,11 +3,13 @@ This function deletes all matching values from an array and returns an array of 
 @param {any} target - The value to be deleted.
 @returns {any[]} An array of the deleted values.
 */
-export function deleteAllByValue(this: Array<any>, target: any): any[] {
-  let index = this.indexOf(target);
-  if (index == -1) {
-  return [];
+export function deleteAllByValue(this: Array<any>, target: any,): any[] {
+  const tempArray = this.map(el=> JSON.stringify(el));
+  const deletedValues: any[] = [];
+  for (let i = tempArray.length - 1; i >= 0; i--) {
+    if (tempArray[i] === JSON.stringify(target)) {
+      deletedValues.push(this.splice(i, 1)[0]);
+    }
   }
-  const deleted = this.splice(index, 1);
-  return deleted.concat(deleteAllByValue.call(this, target));
-  }
+   return deletedValues;
+}
