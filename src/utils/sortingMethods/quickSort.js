0a13db9e8b0053c61d9677fb111b1c0eaeae6f40
Ajulrag.b <ajulrag353@gmail.com>
2023-05-18 11:12:07 +0530

    feat: added quickSort method

---
+++ src/utils/sortingMethods/quickSort.js
@@ -0,0 +1,31 @@
+module.exports = function quickSort(this, isDescend) {
+    if(this.length < 2){
+        return this;
+    }
+    let leftArray = [];
+    let rightArray = [];
+
+    let pivot = this[this.length-1];
+    
+    if (isDescend) {
+        for(let i=0; i<this.length-1 ;i++) {
+            if(this[i] < pivot ) {
+                rightArray.push(this[i])
+            } else {
+                leftArray.push(this[i]);
+            }
+        }
+    } else {
+        for(let i=0; i<this.length-1 ;i++) {
+            if(this[i] > pivot ) {
+                rightArray.push(this[i])
+            } else {
+                leftArray.push(this[i]);
+            }
+        }
+    }
+    const sortedArray= [...quickSort(leftArray),pivot,...quickSort(rightArray)]
+    sortedArray.forEach((el, index)=>{
+        this[index] = el;
+    })
+}
