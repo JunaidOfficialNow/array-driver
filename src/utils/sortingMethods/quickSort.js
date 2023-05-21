169cd4f10707d56dd043ee8b56b6e15ed9e69975
Ajulrag.b <ajulrag353@gmail.com>
2023-05-21 20:53:15 +0530

    feat:added quickSort method

--- src/utils/sortingMethods/quickSort.js
+++ src/utils/sortingMethods/quickSort.js
@@ -1,36 +1,38 @@
 /**
- *  This function sort the original array with quicksort algorithm.
+ * This function sort the original array with quicksort algorithm.
  * @param  isDescend boolean: true for sort in descending order.
  * @returns the sorted array.
  */
 module.exports = function quickSort(this, isDescend) {
-    if(this.length < 2){
+    function sort(this, isDescend) {
+      if (this.length < 2) {
         return this;
-    }
-    let leftArray = [];
-    let rightArray = [];
-
-    let pivot = this[this.length-1];
-    
-    if (isDescend) {
-        for(let i=0; i<this.length-1 ;i++) {
-            if(this[i] < pivot ) {
-                rightArray.push(this[i])
-            } else {
-                leftArray.push(this[i]);
-            }
+      }
+      
+      let leftArray = [];
+      let rightArray = [];
+      let pivot = this[this.length - 1];
+      
+      if (isDescend) {
+        for (let i = 0; i < this.length - 1; i++) {
+          if (this[i] < pivot) { 
+            rightArray.push(this[i]);
+          } else {
+            leftArray.push(this[i]);
+          }
         }
-    } else {
-        for(let i=0; i<this.length-1 ;i++) {
-            if(this[i] > pivot ) {
-                rightArray.push(this[i])
-            } else {
-                leftArray.push(this[i]);
-            }
+      } else {
+        for (let i = 0; i < this.length - 1; i++) {
+          if (this[i] > pivot) {
+            rightArray.push(this[i]);
+          } else {
+            leftArray.push(this[i]);
+          }
         }
+      }
+      
+      return [...sort(leftArray, isDescend), pivot, ...sort(rightArray, isDescend)];
     }
-    const sortedArray= [...quickSort(leftArray),pivot,...quickSort(rightArray)]
-    sortedArray.forEach((el, index)=>{
-        this[index] = el;
-    })
+    const sortedArray = sort(this, isDescend);
+    sortedArray.forEach((el, index)=> this[index]= el)
 }
